eea33dbbf78437267bfc30600e95f3a09ae0ac7e
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2023-11-14 22:21:51 -0600

    Initiated react app

---
+++ creating-components/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function Header(){ // All component names must start with capital letters
+  return <h1>Hello React!</h1>
+}
+
+function App() {
+  //Render our Header component with the root component
+  return <Header />
+}
+
+export default App;
