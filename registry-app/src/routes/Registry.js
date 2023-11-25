b7568625ce3dc21871487725b9574658857b0f25
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2023-11-25 16:46:23 -0600

    implementet useState React Hook

--- registry-app/src/routes/Registry.js
+++ registry-app/src/routes/Registry.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Registry(){
+    const [registryData, setRegistryData] =  useState([]);
+    const [textInput, setTextInput] = useState("");
+
+    const addItem = (e) => {
+        e.preventDefault();
+
+        const tempData = [...registryData];
+        tempData.push(textInput);
+        setRegistryData(tempData);
+        setTextInput("");
+    }
+    console.log(registryData);
+
     return (
         <div>
             <h1>Registry</h1>
             <Link to="/">Click here to go to home</Link>
+            <form onSubmit={addItem}>
+                <label>Text Input:
+                    <input type="text" value={textInput} onChange={(e) => setTextInput(e.target.value)} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
     
